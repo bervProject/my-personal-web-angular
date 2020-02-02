1ede48ea79cfc7c714589a413bdcaf46f4689517
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-02-02 14:42:02 +0700

    Changes to bulma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EducationComponent } from './education/education.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'education', component: EducationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
